34119f7ba3d7f0efaee08ebb30a8b049a8414c3b
Андрей Смирнов <smirnov71@yandex.ru>
2022-11-09 18:02:11 +0300

    pictures!

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,10 +4,11 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
-        labels = {"text": "Текст", "group": "Группа"}
+        labels = {"text": "Текст", "group": "Группа", "image": "Картинка"}
         model = Post
         fields = ('text', 'group', 'image')
         help_texts = {
             "text": "Текст нового поста",
             "group": "Группа, к которой будет относиться пост",
+            "image": "Иллюстрация к посту"
         }
