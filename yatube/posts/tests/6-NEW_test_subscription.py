88b5f3852ceef17e56ce8e7fa70eedaa69468c75
Андрей Смирнов <smirnov71@yandex.ru>
2022-12-26 16:35:08 +0300

    started test 2

---
+++ yatube/posts/tests/6-NEW_test_subscription.py
@@ -0,0 +1,6 @@
+# posts/tests/test_subscription.py
+# test authorized user may subscribe and unsubscribe
+def test_auth_subscribe_unsubscribe():
+
+# test new record appears only for those who subscribed
+def test_new_rec_for_unsubscribed_only ():
